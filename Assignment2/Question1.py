b2d9c71e62347ea459a9f2e8d02932c437d75635
OmBijwani <ombijwani06@gmail.com>
2026-05-20 16:10:52 +0530

    Added assignment 2

--- Assignment2/Question1.py
+++ Assignment2/Question1.py
@@ -6,32 +6,21 @@ student = {
 }
 
 print("Dictionary:", student)
-
-
 student["city"] = "Jaipur"
 student["age"] = 21
-
 print("Updated Dictionary:", student)
-
-
 
 #TUPLE 
 numbers = (10, 20, 30, 40, 50)
 
 print("Tuple:", numbers)
-
-
 print("First Element:", numbers[0])
 print("Length:", len(numbers))
-
 
 #SET 
 fruits = {"apple", "banana", "mango"}
 
 print("Set:", fruits)
-
-
 fruits.add("orange")
 fruits.add("apple")   
-
 print("Updated Set:", fruits)
